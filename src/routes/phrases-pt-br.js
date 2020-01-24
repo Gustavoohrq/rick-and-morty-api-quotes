4224cfd5df8cfb2e06f8e46f137764219aa27dfc
Gustavo Henrique <gustavofmariano@gmail.com>
2020-01-24 16:59:01 -0300

    ...

--- src/routes/phrases-pt-br.js
+++ src/routes/phrases-pt-br.js
@@ -4,10 +4,12 @@ const api = require('../phrases/phrases-pt_br.json')
 
 router.get('/', (req, res) =>{
     res.json(api)
-})
-router.get('/:id', (req, res) =>{
+});
+
+router.get('/:id', (req, res) => {
     var id = parseInt(req.params.id) - 1
     res.json(api.phrases_pt_br[id])
-})
+});
+
 
 module.exports = router;
